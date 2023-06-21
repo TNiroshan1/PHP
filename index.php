6ed5ef1bf11dc519bf132646a786b21ced847980
TNiroshan1 <tniroshan1@yahoo.com>
2023-06-21 20:00:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -16,28 +16,36 @@ $conn = sqlsrv_connect($serverName, $connectionInfo);
 
 echo "Database connection is OK<br>"; 
 
-if(isset($_POST["Device_ID"]) && isset($_POST["Date_Time"]) && isset($_POST["Date_Time"]) && isset($_POST["Channel_ID"])) {
+// if(isset($_POST["Device_ID"]) && isset($_POST["Date_Time"]) && isset($_POST["Date_Time"]) && isset($_POST["Channel_ID"])) {
 
-	$xUnitID = $_POST["Device_ID"];
-	$xdate = $_POST["Date_Time"];
-	$xTime1 = $_POST["Date_Time"];
-	$xChanelID = $_POST["Channel_ID"];
+// 	$xUnitID = $_POST["Device_ID"];
+// 	$xdate = $_POST["Date_Time"];
+// 	$xTime1 = $_POST["Date_Time"];
+// 	$xChanelID = $_POST["Channel_ID"];
 
 	$sql = "INSERT INTO [dbo].[bronze_layer] 
 								
-	    ([UnitID]
+	    (	[ID]
+			,[UnitID]
             ,[Date1]
             ,[Time1]
-            ,[ChanelID])
+            ,[ChanelID]
+			,[Duration])
+			
         VALUES
-            (".$xUnitID.", ".$xdate.",".$xTime1.", ".$xChanelID.")"; 
 
+			(1, 2, ?, ?, 3 , 10)"; 
+echo "INSERT OK<br>";
 	/* if (mysqli_query($conn, $sql)) { 
 		echo "\nNew record created successfully"; 
 	} else { 
 		echo "Error: " . $sql . "<br>" . mysqli_error($conn); 
+
+
+            // (".$xUnitID.", ".$xdate.",".$xTime1.", ".$xChanelID.")"; 
+
 	}*/
-} 
+// } 
 
 
 
