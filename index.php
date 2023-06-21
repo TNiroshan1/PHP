3dd53eb6f1d658e9b3fe610eab8d7518a1f8fb69
TNiroshan1 <tniroshan1@yahoo.com>
2023-06-21 21:04:59 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,32 @@
 <?php
-    $serverName = "iotserver1.database.windows.net"; // update me
-    $connectionOptions = array(
-        "Database" => "iot_db", // update me
-        "Uid" => "iotuser", // update me
-        "PWD" => "UOK@1234" // update me
-    );
-    //Establishes the connection
-    $conn = sqlsrv_connect($serverName, $connectionOptions);
+$serverName = "iotserver1.database.windows.net"; // update me
+$connectionOptions = array(
+	"Database" => "iot_db",
+	// update me
+	"Uid" => "iotuser",
+	// update me
+	"PWD" => "UOK@1234" // update me
+);
+//Establishes the connection
+$conn = sqlsrv_connect($serverName, $connectionOptions);
 
-	if(isset($_POST["Device_ID"]) && isset($_POST["Date_Time"]) && isset($_POST["Channel_ID"])) {
+if (isset($_POST["Device_ID"]) && isset($_POST["Date_Time"]) && isset($_POST["Channel_ID"])) {
 
-		$xUnitID = $_POST["Device_ID"];
-		$xdate = $_POST["Date_Time"];
-		$xTime1 = $_POST["Channel_ID"];
-	
+	$xUnitID = $_POST["Device_ID"];
+	$xdate = $_POST["Date_Time"];
+	$xChanelID = $_POST["Channel_ID"];
 
 
-    $tsql= "INSERT INTO bronze (DeviceID, Date_time, ChannelID)
-  VALUES (".$xUnitID.", ".$xdate.", ".$xChanelID.")";
 
-	}
+	$tsql = "INSERT INTO bronzelayer (DeviceID, Date_time, ChannelID)
+  VALUES (" .$xUnitID. ", " .$xdate. ", " .$xChanelID. ")";
 
-    $getResults= sqlsrv_query($conn, $tsql);
-    echo ("insert data to table" . PHP_EOL);
-    if ($getResults == FALSE)
-        echo (sqlsrv_errors());
-   
-    sqlsrv_free_stmt($getResults);
+}
+
+$getResults = sqlsrv_query($conn, $tsql);
+echo ("insert data to table" . PHP_EOL);
+if ($getResults == FALSE)
+	echo (sqlsrv_errors());
+
+sqlsrv_free_stmt($getResults);
 ?>
