a760f93923e5ee6bd6c3c56a7a542ff2b38cef9d
TNiroshan1 <tniroshan1@yahoo.com>
2023-06-21 20:40:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,57 +1,74 @@
-<?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:iotserver1.database.windows.net,1433; Database = iot_db", "iotuser", "UOK@1234");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
+// // < ?php 
+// // PHP Data Objects(PDO) Sample Code:
+// try {
+// 	$conn = new PDO("sqlsrv:server = tcp:iotserver1.database.windows.net,1433; Database = iot_db", "iotuser", "UOK@1234");
+// 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// } catch (PDOException $e) {
+// 	print("Error connecting to SQL Server.");
+// 	die(print_r($e));
+// }
 
- // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "iotuser", "pwd" => "UOK@1234", "Database" => "iot_db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:iotserver1.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+// // SQL Server Extension Sample Code:
+// $connectionInfo = array("UID" => "iotuser", "pwd" => "UOK@1234", "Database" => "iot_db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+// $serverName = "tcp:iotserver1.database.windows.net,1433";
+// $conn = sqlsrv_connect($serverName, $connectionInfo);
 
-echo "Database connection is OK<br>"; 
+// echo "Database connection is OK<br>";
 
-// if(isset($_POST["Device_ID"]) && isset($_POST["Date_Time"]) && isset($_POST["Date_Time"]) && isset($_POST["Channel_ID"])) {
+// // if(isset($_POST["Device_ID"]) && isset($_POST["Date_Time"]) && isset($_POST["Date_Time"]) && isset($_POST["Channel_ID"])) {
 
-// 	$xUnitID = $_POST["Device_ID"];
-// 	$xdate = $_POST["Date_Time"];
-// 	$xTime1 = $_POST["Date_Time"];
-// 	$xChanelID = $_POST["Channel_ID"];
+// // 	$xUnitID = $_POST["Device_ID"];
+// // 	$xdate = $_POST["Date_Time"];
+// // 	$xTime1 = $_POST["Date_Time"];
+// // 	$xChanelID = $_POST["Channel_ID"];
 
-	$sql = "INSERT INTO dbo.bronze_layer
-	(ID	,UnitID	,Date1	,Time1	,ChanelID ,Duration)
-VALUES
-	(1,1,0,0,1,0)"; 
+// // $sql = "INSERT INTO dbo.bronze_layer
+// // 	(ID	,UnitID	,Date1	,Time1	,ChanelID ,Duration)
+// // VALUES
+// // 	(1,1,0,0,1,0)";
 
-echo "INSERT OK<br>";
-
-
-	//  if $conn, $sql) { 
-	// 	echo "\nNew record created successfully"; 
-	// } else { 
-	// 	echo "Error: " . $sql . "<br>" . mysqli_error($conn); 
+// // echo "INSERT OK<br>";
 
 
-    //         // (".$xUnitID.", ".$xdate.",".$xTime1.", ".$xChanelID.")"; 
+//  if $conn, $sql) { 
+// 	echo "\nNew record created successfully"; 
+// } else { 
+// 	echo "Error: " . $sql . "<br>" . mysqli_error($conn); 
 
-	// }
+
+//         // (".$xUnitID.", ".$xdate.",".$xTime1.", ".$xChanelID.")"; 
+
+// }
 // } 
 
 
-if (sqlsrv_query($conn, $sql) === TRUE) {
-	echo "New record created successfully";
-  } else {
-	echo "Error: " . $sql . "<br>" . $conn_error;
-  }
-  
-  $conn_close();
-  
+// if (sqlsrv_query($conn, $sql) === TRUE) {
+// 	echo "New record created successfully";
+// } else {
+// 	echo "Error: " . $sql . "<br>" . $conn_error;
+// }
+
+// $conn_close();
 
 
 
+
+// ? > //
+<?php
+    $serverName = "iotserver1.database.windows.net"; // update me
+    $connectionOptions = array(
+        "Database" => "iot_db", // update me
+        "Uid" => "iotuser", // update me
+        "PWD" => "UOK@1234" // update me
+    );
+    //Establishes the connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    $tsql= "INSERT INTO bronze (DeviceID, Date_time, ChannelID, Duration)
+  VALUES (11, 'Doe', 11,11)";
+    $getResults= sqlsrv_query($conn, $tsql);
+    echo ("insert data to table" . PHP_EOL);
+    if ($getResults == FALSE)
+        echo (sqlsrv_errors());
+   
+    sqlsrv_free_stmt($getResults);
 ?>
